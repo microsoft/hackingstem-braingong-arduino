ca5f71c4fbb9b848307635266d9613f5d979b084
Jeremy Franklin-Ross <v-jerfra@microsoft.com>
2018-10-11 16:52:20 -0700

    added 5th sensor, removed accel, replaced all senor to sensor

--- BrainGong/BrainGong.ino
+++ BrainGong/BrainGong.ino
@@ -1,60 +1,26 @@
 #include <Wire.h>
 
-// ------------------------- LIS3DH ACCEL
-// TODO Get rid of Accel code
-// List of registers used by accelerometer
-#define LIS3DH_ADDRESS           0x18
-#define LIS3DH_REG_STATUS1       0x07
-#define LIS3DH_REG_WHOAMI        0x0F
-#define LIS3DH_REG_TEMPCFG       0x1F
-#define LIS3DH_REG_CTRL1         0x20
-#define LIS3DH_REG_CTRL3         0x22
-#define LIS3DH_REG_CTRL4         0x23
-#define LIS3DH_REG_OUT_Y_L       0x2A
-#define LIS3DH_REG_OUT_Y_H       0x2B
-
-#define LIS3DH_8G_SCALE_FACTOR  .00024414f
-
-#define LIS3DH_RANGE_8_G         0b10   // +/- 8g
-
 const int dataRate = 10;      // Change this variable to increase the frequency that data is sent to Excel
-float yAxis = 0;
-
-void writeRegister8 (uint8_t reg, uint8_t val){
-  Wire.beginTransmission (LIS3DH_ADDRESS);
-  Wire.write (reg);
-  Wire.write (val);
-  Wire.endTransmission();
-}
-
-uint8_t readRegister8 (uint8_t reg){
-  Wire.beginTransmission (LIS3DH_ADDRESS);
-  Wire.write (reg);
-  Wire.endTransmission();
-
-  Wire.requestFrom(LIS3DH_ADDRESS, 1);
-  uint8_t val = Wire.read();
-  return val;
-  Wire.endTransmission();
-}
-// ------------------------- END LIS3DH ACCEL
-
 
 // Program variables ----------------------------------------------------------
-int brainSenorPin1 = A0;
-int brainSenorPin2 = A1;
-int brainSenorPin3 = A2;
-int brainSenorPin4 = A3;
+int brainSensorPin1 = A0;
+int brainSensorPin2 = A1;
+int brainSensorPin3 = A2;
+int brainSensorPin4 = A3;
+int brainSensorPin5 = A4;
 
-int brainSenor1;
-int brainSenor2;
-int brainSenor3;
-int brainSenor4;
+int brainSensor1;
+int brainSensor2;
+int brainSensor3;
+int brainSensor4;
+int brainSensor5;
 
-int brainSenor1Tare;
-int brainSenor2Tare;
-int brainSenor3Tare;
-int brainSenor4Tare;
+int brainSensor1Tare;
+int brainSensor2Tare;
+int brainSensor3Tare;
+int brainSensor4Tare;
+int brainSensor5Tare;
+
 
 // Excel variables ------------------------------------------------------------
 String incomingExcelString = "";
@@ -73,45 +39,12 @@ unsigned long serialPreviousTime; // Timestamp to track serial interval
 
 // SETUP ----------------------------------------------------------------------
 void setup() {
-                                              //Initilize LIS3DH and set to +/- 16g Scale Factor
-  Wire.begin();
-
-  Wire.beginTransmission(LIS3DH_ADDRESS);   //Connects to LIS3DH via i2c
-  Wire.write (LIS3DH_REG_WHOAMI);            //Check that board is connected
-  Wire.endTransmission(true);
-  Wire.requestFrom (LIS3DH_ADDRESS, 1);
-  uint8_t deviceID = Wire.read();
-
-  while (deviceID != 0x33){
-    delay(1);
-  }
-
-  writeRegister8 (LIS3DH_REG_CTRL1, 0x07);      //Turn on all axes and set to normal mode
-
-  //set data rate
-  uint8_t accelDataRate = readRegister8 (LIS3DH_REG_CTRL1);
-  accelDataRate &= ~(0xF0);
-  accelDataRate |= 0b0111 << 4;                //Change variable to write
-  writeRegister8 (LIS3DH_REG_CTRL1, accelDataRate);      //Set data rate to 400 mHz, used to manage power consuption
-
-
-  writeRegister8 (LIS3DH_REG_CTRL4, 0x88);      //Enebles High Res and BDU
-
-  writeRegister8 (LIS3DH_REG_CTRL3, 0x10);       // DRDY on INT1
-
-  writeRegister8 (LIS3DH_REG_TEMPCFG, 0x80);      //Activate ADC outputs
-
-  //Set read scale
-  uint8_t rangeControl = readRegister8 (LIS3DH_REG_CTRL4);
-  rangeControl &= ~(0x30);
-  rangeControl |= LIS3DH_RANGE_8_G << 4;                //Change variable to write make sure to also update the scale factor
-  writeRegister8 (LIS3DH_REG_CTRL4, rangeControl);
-
   //Tare all sensors to zero
-  brainSenor1Tare = tare(brainSenorPin1);
-  brainSenor2Tare = tare(brainSenorPin2);
-  brainSenor3Tare = tare(brainSenorPin3);
-  brainSenor4Tare = tare(brainSenorPin4);
+  brainSensor1Tare = tare(brainSensorPin1);
+  brainSensor2Tare = tare(brainSensorPin2);
+  brainSensor3Tare = tare(brainSensorPin3);
+  brainSensor4Tare = tare(brainSensorPin4);
+  brainSensor5Tare = tare(brainSensorPin5);
 
   // Initializations occur here
   Serial.begin(9600);  
@@ -120,21 +53,6 @@ void setup() {
 // START OF MAIN LOOP --------------------------------------------------------- 
 void loop()
 {
-    Wire.beginTransmission(LIS3DH_ADDRESS);
-    Wire.write(LIS3DH_REG_OUT_Y_L | 0x80);
-    Wire.endTransmission();
-
-    Wire.requestFrom(LIS3DH_ADDRESS, 2);
-    while (Wire.available() < 2);
-
-    uint8_t yla = Wire.read();
-    uint8_t yha = Wire.read();
-
-
-    yAxis = yha << 8 | yla;
-
-    yAxis = yAxis * LIS3DH_8G_SCALE_FACTOR;
-  
   // Process sensors
   processSensors();
 
@@ -150,15 +68,16 @@ void loop()
 void processSensors() 
 {
   // Add sensor processing code here
-  brainSenor1 = analogRead( brainSenorPin1 ) - brainSenor1Tare;
-  brainSenor2 = analogRead( brainSenorPin2 ) - brainSenor2Tare;
-  brainSenor3 = analogRead( brainSenorPin3 ) - brainSenor3Tare;
-  brainSenor4 = analogRead( brainSenorPin4 ) - brainSenor4Tare;
+  brainSensor1 = analogRead( brainSensorPin1 ) - brainSensor1Tare;
+  brainSensor2 = analogRead( brainSensorPin2 ) - brainSensor2Tare;
+  brainSensor3 = analogRead( brainSensorPin3 ) - brainSensor3Tare;
+  brainSensor4 = analogRead( brainSensorPin4 ) - brainSensor4Tare;
+  brainSensor5 = analogRead( brainSensorPin5 ) - brainSensor5Tare;
 
-//  brainSenor1 = map(brainSenor1, 0, 512, 0, 100);
-//  brainSenor2 = map(brainSenor2, 0, 512, 0, 100);
-//  brainSenor3 = map(brainSenor3, 0, 512, 0, 100);
-//  brainSenor4 = map(brainSenor4, 0, 512, 0, 100);
+//  brainSensor1 = map(brainSensor1, 0, 512, 0, 100);
+//  brainSensor2 = map(brainSensor2, 0, 512, 0, 100);
+//  brainSensor3 = map(brainSensor3, 0, 512, 0, 100);
+//  brainSensor4 = map(brainSensor4, 0, 512, 0, 100);
 }
 
 // Add any specialized methods and processing code here
@@ -188,19 +107,19 @@ void sendDataToSerial()
   // Send data out separated by a comma (kDelimiter)
   // Repeat next 2 lines of code for each variable sent:
 
-  Serial.print(brainSenor1);
+  Serial.print(brainSensor1);
   Serial.print(kDelimiter);
 
-  Serial.print(brainSenor2);
+  Serial.print(brainSensor2);
   Serial.print(kDelimiter);
 
-  Serial.print(brainSenor3);
+  Serial.print(brainSensor3);
   Serial.print(kDelimiter);
 
-  Serial.print(brainSenor4);
+  Serial.print(brainSensor4);
   Serial.print(kDelimiter);
 
-  Serial.print(yAxis);
+  Serial.print(brainSensor5);
   Serial.print(kDelimiter);
 
   Serial.println(); // Add final line ending character only once
